487cc3ab7e2bd4a194903fc73f0c2b84f1d491b4
gamil91 <alexis_gamil99@yahoo.com.com>
2020-11-01 08:58:28 -0800

    done

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,5 +4,25 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :users, through: :comments
 
+  accepts_nested_attributes_for :categories
+  accepts_nested_attributes_for :users
+
+  def categories_attributes=(category_attributes)
+    category_attributes.values.each do |category_attribute|
+      category_attribute.values.each do |attr|
+        if attr != ""
+        category = Category.find_or_create_by(category_attribute)
+        self.post_categories.build(category_id:category[:id])
+        end
+      end
+    end
+  end
+
+  def users_attributes
+  end
+
+   
+
+
 
 end
